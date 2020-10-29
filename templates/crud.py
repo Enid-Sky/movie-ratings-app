c094c864c8146697f6e23d4482d46abd7c69bacf
Enid-Sky <enid.sky@protonmail.com>
2020-10-29 12:01:41 -0700

    add create user function

---
+++ templates/crud.py
@@ -0,0 +1,14 @@
+"""CRUD operations"""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
