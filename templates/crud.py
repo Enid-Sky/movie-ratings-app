dfdb825d09da25086413b2f20ad3ab75160d4fe4
Enid-Sky <enid.sky@protonmail.com>
2020-10-29 12:02:14 -0700

    add create movie function

--- templates/crud.py
+++ templates/crud.py
@@ -12,3 +12,15 @@ def create_user(email, password):
     db.session.commit()
 
     return user
+
+
+def create_movie(title, overview, release_date, poster_path):
+    """Creates a movie """
+
+    movie = Movie(title=title, overview=overview,
+                  release_date=release_date, poster_path=poster_path)
+
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
