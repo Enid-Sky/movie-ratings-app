a61b770065eb3464f0191f4d4525f8b30bd8c4dc
Enid-Sky <enid.sky@protonmail.com>
2020-10-29 18:15:01 -0700

    git init

---
+++ seed_database.py
@@ -0,0 +1,57 @@
+"""Script to seed database."""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for movie in movie_data:
+
+    # TODO: get the title, overview, and poster_path from the movie
+    # dictionary.
+    title, overview, poster_path = (
+        movie['title'], movie['overview'], movie['poster_path'])
+
+    # Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+
+    release_date = movie['release_date']
+    date = datetime.strptime(release_date, "%Y-%m-%d")
+
+    # TODO: create a movie here and append it to movies_in_db
+
+    movie = crud.create_movie(
+        title=title, overview=overview, poster_path=poster_path, release_date=date)
+    movies_in_db.append(movie)
+
+
+for n in range(10):
+    email = f'user{n}@test.com'  # Voila! A unique email!
+    password = 'test'
+
+
+# TODO: create a user here
+    user = crud.create_user(email, password)
+
+    # TODO: create 10 ratings for the user
+    for individual_rating in range(10):
+        movie_choice = choice(movies_in_db)
+        score = randint(1, 5)
+        crud.create_rating(user, movie_choice, score)
